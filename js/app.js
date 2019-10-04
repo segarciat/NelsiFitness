be98da3a9869d3b95147ccbee9125776ab022062
Sergio Garcia Tapia <gsergio488@gmail.com>
2019-10-03 21:38:45 -0400

    Added smooth scrolling

--- js/app.js
+++ js/app.js
@@ -1,18 +1,29 @@
 navwrap = document.querySelector('.navbar-wrap');
+// Get nav links
+const navLinks = Array.from(document.querySelectorAll('.navbar-menu a'));
+// Get the sections, links
+const sections = Array.from(document.querySelectorAll('section'));
+sections.unshift(document.querySelector('header'));
 
 window.addEventListener('scroll', () => {
   updateCurrent();
   adjustNavbar();
 });
 
-function updateCurrent() {
-  // Get the sections, links
-  const sections = Array.from(document.querySelectorAll('section'));
-  sections.unshift(document.querySelector('header'));
+navwrap.addEventListener('click', smoothScroll);
 
-  // Get nav links and nav height
-  const navLinks = document.querySelectorAll('.navbar-menu a');
-  
+function smoothScroll(e) {
+  if(e.target.hasAttribute('href')){
+    const section = navLinks.indexOf(e.target);
+    sections[section].scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  }
+  e.preventDefault();
+}
+
+function updateCurrent() {
   // Find current section -- start at last
   let current = sections.length - 1;
   while(sections[current].offsetTop > window.scrollY + navwrap.clientHeight)
@@ -32,11 +43,13 @@ function updateCurrent() {
 function adjustNavbar() {
   if(window.scrollY > 0)
   {
+    // Shrink navbar and opaque background
     navwrap.querySelector('.logo').style.height = '4rem';
     navwrap.style.padding = '0.25rem 0';
     navwrap.parentElement.parentElement.style.background = 'rgba(0,0,0,1)';
   }
   else {
+    // More padding and transparent
     navwrap.querySelector('.logo').removeAttribute('style');
     navwrap.removeAttribute('style');
     navwrap.parentElement.parentElement.removeAttribute('style');
